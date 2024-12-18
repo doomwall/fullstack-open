3fc970a627b1f168adc1df09700f9d97eada4eb8
doomwall <tuomo.valkila@gmail.com>
2024-12-18 16:33:26 +0200

    assignment 7

--- osa1/tehtavat6-14/src/App.jsx
+++ osa1/tehtavat6-14/src/App.jsx
@@ -1,19 +1,6 @@
-import { useState } from 'react'
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
-  )
-}
+
+import { useState } from 'react'
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
@@ -21,34 +8,94 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const Display = (props) => {
+  return (
+    <div>
+      <p>Good {props.good}</p>
+      <p>Neutral {props.neutral}</p>
+      <p>Bad {props.bad}</p>
+      <p>All {props.allReviews.length}</p>
+      <p>Average {props.average}</p>
+      <p>Positive {props.positives} %</p>
+    </div>
+  )
+}
+
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allReviews, setAll] = useState([])
+  const [average, setAverage] = useState(0.00)
+  const [allPositives, setAllPositives] = useState([])
+  const [positives, setPositives] = useState(0.00)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-    const updatedLeft = left + 1
-    setTotal(updatedLeft + right)
+  const handleGood = () => {
+    setGood(good + 1)
+
+    const updatedAll = allReviews.concat(1)
+    const updatedPositives = allPositives.concat(1)
+    setAll(updatedAll)
+    setAllPositives(updatedPositives)
+
+    calculateAverage(updatedAll)
+    calculatePositives(updatedAll, updatedPositives)
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
-    const updatedRight = right + 1
-    setTotal(left + updatedRight)
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+
+    const updatedAll = allReviews.concat(0)
+    const updatedPositives = allPositives
+    setAll(updatedAll)
+    setAllPositives(updatedPositives)
+
+    calculateAverage(updatedAll)
+    calculatePositives(updatedAll, updatedPositives)
   }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+    
+    const updatedAll = allReviews.concat(-1)
+    const updatedPositives = allPositives
+    setAll(updatedAll)
+    setAllPositives(updatedPositives)
+
+    calculateAverage(updatedAll)
+    calculatePositives(updatedAll, updatedPositives)
+  }
+
+  const calculateAverage = (updatedAll) => {
+    let summa = 0
+    updatedAll.forEach (value => {
+      summa += value
+    })
+    console.log(summa)
+    setAverage(summa / updatedAll.length)
+  }
+
+  const calculatePositives = (updatedAll, updatedPositives) => {
+    setPositives(updatedPositives.length / updatedAll.length * 100
+    )
+  }
+
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
-      <p>total {total}</p>
+      <h1>Give Feedback</h1>
+      <Button handleClick={handleGood} text='Good' />
+      <Button handleClick={handleNeutral} text='Neutral' />
+      <Button handleClick={handleBad} text='Bad' />
+      <h1>Statistics</h1>
+      <Display 
+        good={good} 
+        neutral={neutral} 
+        bad={bad} 
+        allReviews={allReviews} 
+        average={average}
+        positives={positives}
+         />
     </div>
   )
 }
