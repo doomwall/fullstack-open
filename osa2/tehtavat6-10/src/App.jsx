caa989470f60d37c7e75b51fca306fbc695e3cde
doomwall <tuomo.valkila@gmail.com>
2025-08-06 22:04:16 +0300

    tehtava 6

--- osa2/tehtavat6-10/src/App.jsx
+++ osa2/tehtavat6-10/src/App.jsx
@@ -1,24 +1,52 @@
 import { useState } from 'react'
 
+const Name = (props) => {
+  return (
+    <li>
+      {props.name}
+    </li>
+  )
+}
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: String(persons.length + 1),
+    }
+    console.log(personObject)
+
+    setPersons(persons.concat(personObject))
+    setNewName("")
+  }
+
+const handleNoteChange = (event) => {
+  setNewName(event.target.value)
+}
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <div>debug: {newName}</div>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: 
+          <input value = {newName}
+          onChange={handleNoteChange}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">
+            add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => <Name key={person.id} name={person.name} />) }
+      </ul>
     </div>
   )
 
