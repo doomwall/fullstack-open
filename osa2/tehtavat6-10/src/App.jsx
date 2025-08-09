ee3433642b5068fac5d4947c06dd71553d308696
doomwall <tuomo.valkila@gmail.com>
2025-08-09 14:24:52 +0300

    tehtavat 6-10 done

--- osa2/tehtavat6-10/src/App.jsx
+++ osa2/tehtavat6-10/src/App.jsx
@@ -3,14 +3,66 @@ import { useState } from 'react'
 const Name = (props) => {
   return (
     <li>
-      {props.name}
+      {props.name} {props.number}
     </li>
   )
 }
 
+const Filter = (props) => {
+  return (
+    <div>
+        filter shown with 
+        <input value={props.search}
+        onChange={props.function}/>
+    </div>
+  )
+}
+
+const PersonForm = (props) => {
+  return (
+    <form onSubmit={props.onSubmit}>
+      <div>
+        name: 
+        <input value = {props.nameValue}
+        onChange={props.nameOnChange}/>
+      </div>
+      <div>
+        number: 
+        <input value = {props.numberValue}
+        onChange={props.numberOnChange}/>
+      </div>
+      <div>
+        <button type="submit">
+          add</button>
+      </div>
+    </form>
+  )
+}
+
+const filtering = (list, query) => {
+  return list.filter(person => person.name.toLowerCase().includes(query.toLowerCase()))
+}
+
+
+
 const App = () => {
-  const [persons, setPersons] = useState([{name: "asdf", id: "1"}]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
+
+  const toShow = filtering(persons, search)
+
+  const emptyTemplate = () => {
+    setNewName("")
+    setNewNumber("")
+    setSearch("")
+  }
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -21,42 +73,49 @@ const App = () => {
     
     if (match) {
       alert(`${newName} is already added to phonebook`)
-      setNewName("")
+      emptyTemplate()
       return
     }
 
     const personObject = {
       name: newName,
+      number: newNumber,
       id: String(persons.length + 1),
     }
     console.log(personObject)
 
     setPersons(persons.concat(personObject))
-    setNewName("")
+    emptyTemplate()
   }
 
-  const handleNoteChange = (event) => {
+  const handleNoteChangeName = (event) => {
     setNewName(event.target.value)
   }
+
+  const handleNoteChangeNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleNoteChangeSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>debug: {newName}</div>
-      <form onSubmit={addPerson}>
-        <div>
-          name: 
-          <input value = {newName}
-          onChange={handleNoteChange}/>
-        </div>
-        <div>
-          <button type="submit">
-            add</button>
-        </div>
-      </form>
+      <Filter search={search} function={handleNoteChangeSearch} />
+      <h2>Add new person</h2>
+      <PersonForm 
+        onSubmit={addPerson} 
+        nameValue={newName} 
+        nameOnChange={handleNoteChangeName} 
+        numberValue={newNumber}
+        numberOnChange={handleNoteChangeNumber}
+        />
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <Name key={person.id} name={person.name} />) }
+        {toShow.map(person => <Name key={person.id} name={person.name} number={person.number} />) }
       </ul>
     </div>
   )
