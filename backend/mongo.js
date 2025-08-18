ea455ec4cc48d6c80e7311da2bc73964b598d901
doomwall <tuomo.valkila@gmail.com>
2025-08-18 19:22:36 +0300

    changes to folders

--- backend/mongo.js
+++ backend/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://tuomov:${password}@cluster0.ywpqbda.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://doomwall:${password}@cluster0.ywpqbda.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
@@ -19,12 +19,25 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
+/*
+
 const note = new Note({
-  content: 'HTML is easy',
-  important: true,
-})
+  content: 'No, this is note',
+  important: false,
+}) 
+  
+
 
 note.save().then(result => {
   console.log('note saved!')
+  mongoose.connection.close()
+})
+
+*/
+
+Note.find({important: true}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
   mongoose.connection.close()
 })
