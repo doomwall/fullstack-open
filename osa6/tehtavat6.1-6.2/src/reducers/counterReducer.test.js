5b26fbff353dcf63e9a1a5ac3b70d40cb8b12e8a
doomwall <tuomo.valkila@gmail.com>
2025-12-05 00:20:00 +0200

    tehtavat 6.1 - 6.2

--- osa6/tehtavat6.1-6.2/src/reducers/counterReducer.test.js
+++ osa6/tehtavat6.1-6.2/src/reducers/counterReducer.test.js
@@ -32,4 +32,60 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+    test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+    test('numbers are reset', () => {
+    const action1 = {
+      type: 'RESET'
+    }
+    const action2 = {
+      type: 'GOOD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+
+    const newState2 = counterReducer(state, action2)
+    expect(newState2).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0
+    })
+
+    const newState = counterReducer(state, action1)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
 })
